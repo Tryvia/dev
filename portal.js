4d31f877c8a4ceeb10d9ac8e84f0a8e0344c6944
Tryvia <victor.bg.quintanilha@gmail.com>
2025-09-22 15:28:55 -0300

    Update portal.js

--- portal.js
+++ portal.js
@@ -10,7 +10,7 @@ try {
   permissoes = [];
 }
 if (!sessionStorage.getItem('tryvia_logged')) {
-//window.location.href = 'https://tryvia.github.io/TryviaBI/tryvia_bi_login%20(1).html';
+window.location.href = 'https://tryvia.github.io/dev/tryvia_portal_dev.html';
 sessionStorage.setItem('tryvia_logged', 'true');
 }
    // Função para exibir apenas a data (dd/mm/aaaa), ignorando horário UTC
@@ -6952,7 +6952,7 @@ document.addEventListener('DOMContentLoaded', function() {
 function logoutTryvia() {
     sessionStorage.removeItem('tryvia_logged');
     localStorage.removeItem('username');
-   // window.location.href = 'https://tryvia.github.io/TryviaBI/tryvia_bi_login%20(1).html';
+    window.location.href = 'https://tryvia.github.io/dev/tryvia_portal_dev.html';
 }
 
 // ===== FUNÇÕES PARA GERENCIAR USUÁRIOS =====
@@ -10957,5 +10957,6 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     }
 });
+
 
 
